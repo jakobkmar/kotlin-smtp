c05d93f89eef218ba5d4c2a265c7399c778db298
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-04 23:08:11 +0200

    Update SmtpCommand.kt

--- src/main/kotlin/net/axay/kotlinsmtp/server/command/api/SmtpCommand.kt
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/api/SmtpCommand.kt
@@ -1,7 +1,14 @@
 package net.axay.kotlinsmtp.server.command.api
 
 import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
 
-interface SmtpCommand {
-    fun execute(command: String, session: SmtpSession)
+abstract class SmtpCommand(
+    private val name: String,
+    private val expectedSyntax: String,
+) {
+    abstract suspend fun execute(command: ParsedCommand, session: SmtpSession)
+
+    protected suspend fun SmtpSession.respondSyntax(message: String = "Syntax error in parameters or arguments") =
+        sendResponse(SmtpStatusCode.CommandSyntaxError, "$message - Expected syntax: $name $expectedSyntax")
 }
