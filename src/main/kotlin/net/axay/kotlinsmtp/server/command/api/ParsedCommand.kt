b4cc900cfc00ededb582858fd708bbe94981c112
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 00:45:09 +0200

    Update ParsedCommand.kt

--- src/main/kotlin/net/axay/kotlinsmtp/server/command/api/ParsedCommand.kt
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/api/ParsedCommand.kt
@@ -2,7 +2,7 @@ package net.axay.kotlinsmtp.server.command.api
 
 import net.axay.kotlinsmtp.Values
 
-class ParsedCommand(private val rawCommand: String) {
+class ParsedCommand(val rawCommand: String) {
     val commandName = rawCommand.takeWhile { it != ' ' }.toUpperCase()
 
     val parts by lazy { rawCommand.split(Values.whitespaceRegex) }
