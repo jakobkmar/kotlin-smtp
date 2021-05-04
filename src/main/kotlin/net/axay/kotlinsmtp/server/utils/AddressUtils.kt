84a7e867cbd8af91815f384596ab777eaa1602a2
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 01:18:58 +0200

    Update AddressUtils.kt

--- src/main/kotlin/net/axay/kotlinsmtp/server/utils/AddressUtils.kt
+++ src/main/kotlin/net/axay/kotlinsmtp/server/utils/AddressUtils.kt
@@ -1,5 +1,9 @@
 package net.axay.kotlinsmtp.server.utils
 
 object AddressUtils {
+    private val addressRegex = Regex("([A-Za-z.0-9]+)(@)([A-Za-z0-9]+\\.)+([A-Za-z]+)")
+
     fun removeBrackets(address: String) = address.removePrefix("<").removeSuffix(">")
+
+    fun validateAddress(address: String) = addressRegex.matches(address)
 }
