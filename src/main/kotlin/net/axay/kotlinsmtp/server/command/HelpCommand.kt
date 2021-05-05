09eaf84df8dc0c3455bf80786488b00d23c07f35
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 23:10:41 +0200

    Create HelpCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/HelpCommand.kt
@@ -0,0 +1,28 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommands
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class HelpCommand : SmtpCommand(
+    "HELP",
+    "Displays helpful information about the server in general, or about the given (optional) search term.",
+    "(searchterm)"
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        val searchterm = command.rawWithoutCommand
+
+        if (searchterm.isNotEmpty()) {
+            val searchedCommand = SmtpCommands.values().find { it.name.contains(searchterm) }
+            if (searchedCommand != null) {
+                session.sendResponse(SmtpStatusCode.HelpMessage, searchedCommand.instance.description)
+            } else {
+                session.sendResponse(SmtpStatusCode.HelpMessage, "The given searchterm / command was not found.")
+            }
+        } else {
+            session.sendResponse(SmtpStatusCode.HelpMessage, "Issue 'HELP searchterm' to get more information about a specific command.")
+        }
+    }
+}
