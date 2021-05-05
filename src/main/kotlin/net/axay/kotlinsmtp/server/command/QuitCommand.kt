32ca481cdb0a11a3f913d5b77c1966b586c94c43
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 23:10:47 +0200

    Create QuitCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/QuitCommand.kt
@@ -0,0 +1,16 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class QuitCommand : SmtpCommand(
+    "QUIT",
+    "Closes the current session."
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        session.sendResponse(SmtpStatusCode.ServiceClosingChannel, "Ok")
+        session.shouldQuit = true
+    }
+}
