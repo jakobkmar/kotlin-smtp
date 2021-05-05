40ae5ad279c3b3056e22875f6982b788f8a55e12
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 19:58:40 +0200

    Create ExpnCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/ExpnCommand.kt
@@ -0,0 +1,20 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class ExpnCommand : SmtpCommand(
+    "EXPN",
+    "Checks if the given mailing list exists (and if so, this may return the membership)",
+    "searchterm"
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        val searchterm = command.rawWithoutCommand
+
+        // TODO add implementation when user storage is added
+
+        session.sendResponse(SmtpStatusCode.CommandNotImplemented, "The expand command is not supported")
+    }
+}
