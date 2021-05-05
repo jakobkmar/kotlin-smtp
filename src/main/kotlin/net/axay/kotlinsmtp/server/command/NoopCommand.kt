c876e494b10ef7c6721cbb8691f77fcf6a4d9b3c
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 23:10:44 +0200

    Create NoopCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/NoopCommand.kt
@@ -0,0 +1,15 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class NoopCommand : SmtpCommand(
+    "NOOP",
+    "This command will lead to no operations being issued."
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        session.sendResponse(SmtpStatusCode.Okay, "Ok")
+    }
+}
