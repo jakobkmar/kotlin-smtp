fdce4fcfb0fa25f93a9ad479afbd840f16397370
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 19:25:21 +0200

    Create RsetCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/RsetCommand.kt
@@ -0,0 +1,20 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class RsetCommand : SmtpCommand(
+    "RSET",
+    "Resets the current session."
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        if (command.parts.size != 1)
+            respondSyntax()
+
+        session.resetTransaction()
+
+        session.sendResponse(SmtpStatusCode.Okay, "Ok")
+    }
+}
