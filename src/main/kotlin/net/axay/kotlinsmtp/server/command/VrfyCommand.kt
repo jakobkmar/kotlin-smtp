9380e4424c2b6302041b2d96da733f2cca864825
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-05 19:52:23 +0200

    Create VrfyCommand.kt

---
+++ src/main/kotlin/net/axay/kotlinsmtp/server/command/VrfyCommand.kt
@@ -0,0 +1,20 @@
+package net.axay.kotlinsmtp.server.command
+
+import net.axay.kotlinsmtp.server.SmtpSession
+import net.axay.kotlinsmtp.server.command.api.ParsedCommand
+import net.axay.kotlinsmtp.server.command.api.SmtpCommand
+import net.axay.kotlinsmtp.server.utils.SmtpStatusCode
+
+class VrfyCommand : SmtpCommand(
+    "VRFY",
+    "Checks if a given mailbox exists.",
+    "searchterm"
+) {
+    override suspend fun execute(command: ParsedCommand, session: SmtpSession) {
+        val searchTerm = command.rawCommand.removeRange(0, 5)
+
+        // TODO add implementation when user storage is added
+
+        session.sendResponse(SmtpStatusCode.CommandNotImplemented, "The verify command is not supported")
+    }
+}
