4ba916648c99467294652e9fac99ce338d8faca1
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-08 01:00:31 +0200

    Update SmtpTransactionHandler.kt

--- src/main/kotlin/net/axay/kotlinsmtp/server/SmtpTransactionHandler.kt
+++ src/main/kotlin/net/axay/kotlinsmtp/server/SmtpTransactionHandler.kt
@@ -1,6 +1,9 @@
 package net.axay.kotlinsmtp.server
 
 abstract class SmtpTransactionHandler {
+    lateinit var sessionData: SmtpSession.SessionData
+        internal set
+
     open suspend fun from(sender: String) { }
 
     open suspend fun to(recipient: String) { }
